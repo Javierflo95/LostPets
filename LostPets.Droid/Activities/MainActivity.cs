22e8abb404f0d6116b4332a69c09589792e05d79
JAVIER ARTURO FLORIAN FRANCO <javierflo.95@hotmail.com>
2018-03-21 00:55:11 -0500

    Add project files.

---
+++ LostPets.Droid/Activities/MainActivity.cs
@@ -0,0 +1,36 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace LostPets.Droid
+{
+    [Activity(Label = "Lost Pets", MainLauncher = true, Icon = "@mipmap/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+        Button button;
+        EditText editText;
+        TextView txtView;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.Main);
+            //We create the control instances
+            button = FindViewById<Button>(Resource.Id.myButton);
+            editText = FindViewById<EditText>(Resource.Id.txtEdit);
+            txtView = FindViewById<TextView>(Resource.Id.lblNombre);
+            //click event
+            button.Click += delegate
+            {
+                if (!string.IsNullOrWhiteSpace(editText.Text))
+                    txtView.Text = editText.Text;
+                else
+                    button.Text = "Favor ingrese su nombre!.";
+            };
+
+        }
+    }
+}
+
